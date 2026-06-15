14c0918648a42cb6c824f05d64388b22c96e5bae
Allen Peter <allen.peter@netcracker.com>
2026-06-15 10:38:14 +0530

    About page

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.html',
   styleUrl: './header.less',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    RouterLink
   ],
   standalone: true
 })
